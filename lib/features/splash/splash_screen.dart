4fafdcf0a96316419d9e7393a13af15505b27ec3
이진경 <spontaneous@ijingyeong-ui-MacBookAir.local>
2025-05-21 22:02:48 +0900

    splash_screen 이미지 업데이트 완료

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, '/home');
     });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: Text(
-          'LIGHT WESTERN FOOD',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        child: Image(
+          image: AssetImage('assets/images/splash_screen.png'),
+          fit: BoxFit.contain,
         ),
       ),
     );
