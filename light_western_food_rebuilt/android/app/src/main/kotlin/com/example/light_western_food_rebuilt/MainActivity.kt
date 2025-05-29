f669686b9f9087ff5b73858533b9d6ec3e84594f
KTheES <keunsu2889@gmail.com>
2025-05-29 19:59:34 +0900

    commit

--- light_western_food_rebuilt/android/app/src/main/kotlin/com/example/light_western_food_rebuilt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.light_western_food_rebuilt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\light_western_food"
+export "FLUTTER_APPLICATION_PATH=C:\light_western_food\light_western_food_rebuilt"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
