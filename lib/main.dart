8c028afcd60f1eb4a4eae7d87963e423677a5fe1
KTheES <keunsu2889@gmail.com>
2025-06-07 03:25:03 +0900

    hive and firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'config/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter(); // Hive 초기화
+
+  await Hive.openBox('todoBox'); // Hive 박스 열기- 투두 저장용입니다.
+
   runApp(const InitApp());
 }
 
