9dff85fcac231f48ef01d917d76b4d9351b5f220
이진경 <spontaneous@ijingyeong-ui-MacBookAir.local>
2025-06-20 22:13:37 +0900

    투두 필터 수정 완료, main.dart-Firebase 연동 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'config/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';             // ✅ 추가
+import 'package:light_western_food/firebase_options.dart';
+import 'features/home/home_screen.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,17 +32,14 @@ class InitApp extends StatelessWidget {
     );
   }
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To-Do List With Your Pet',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.splash,
-      routes: AppRoutes.routes,
+      home: HomeScreen(),
     );
   }
 }
